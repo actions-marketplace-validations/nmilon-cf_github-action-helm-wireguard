96951c788e91a225ea939b400e66414307c932b7
nmilon <nicolas.milon@connecting-food.com>
2021-07-03 18:23:35 +0200

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,20 @@ then
     echo "No VPN config given, not starting tunnel"
 else
     echo "Starting VPN tunnel"
-    sudo bash -c "echo $WIREGUARD_CONFIG > /etc/wireguard/tunnel.conf"
-    sudo wg-quick up tunnel;
+    echo $WIREGUARD_CONFIG > /etc/wireguard/tunnel.conf
+    wg-quick up tunnel;
 fi
 echo $KUBECONFIG > ~/.kube/config;
-echo $EXEC | bash
+echo "$EXEC" > run.sh
+chmod +x ./run.sh
+echo "Executing script"
+
+./run.sh
+
+if [ -z "$WIREGUARD_CONFIG" ] 
+then 
+    echo "No VPN config given, not stopping tunnel"
+else
+    echo "Stopping VPN tunnel"
+    wg-quick down tunnel;
+fi
